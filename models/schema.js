c8b0c66e73781a027e7068f0044e50244716a6f7
Caitlin <caitlinsweeneylmt@gmail.com>
2018-04-02 20:51:19 -0700

    basic server

---
+++ models/schema.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
