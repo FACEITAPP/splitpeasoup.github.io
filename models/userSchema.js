f5b9ed1760ddf3d91e871620556cd7b01475a49c
scubaguypnw <scubaphoto@gmail.com>
2018-04-03 20:50:07 -0700

    built out authorization methods for mongo schemas -3

--- models/userSchema.js
+++ models/userSchema.js
@@ -22,4 +22,37 @@ const userSchema = Schema({
 
 const User = mongoose.model('User', userSchema);
 
+
+userSchema.pre('save', function (next) {
+  let user = this;
+  if (user.isNew) {
+    bcrypt.has(user.password, 10, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      user.passwordHash = hash;
+      next();
+    })
+  } else {
+    console.log(user, 'is not new!')
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function (password) {
+  let user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, user.password, (err, valid) => {
+      if (err) {
+        reject(err);
+      }
+      if (!valid) {
+        reject(err);
+      }
+      let payload = { userID: user.id };
+      let token = jwt.sign(payload, process.env.SECRET);
+      resolve(token);
+    });
+  });
+};
+
 module.exports = User;
