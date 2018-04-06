947915ceb95c70a007c54f85baaec0aab5196661
alicialycan <alicialycan@gmail.com>
2018-04-05 21:22:06 -0700

    add error checks and start script

---
+++ test/dotenv.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// require('dotenv').config();
+
+// describe('.env file', () => {
+//   test('should allow access to each property', () => {
+//    expect(process.env.PORT).toEqual('3000');
+//    expect(process.env.APP_SECRET).toEqual('ilovemountains');
+//    expect(process.env.MONGODB_URI).toEqual('mongodb://localhost/face');
+//   })
+// });
