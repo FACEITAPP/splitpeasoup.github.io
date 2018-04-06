25e53db3213380392acffbc3b1b6b34b41cc09dc
alicialycan <alicialycan@gmail.com>
2018-04-05 19:39:57 -0700

    basic and bearer auth all working!

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 'use strict';
-require('dotenv').config(); 
+require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/userRouter.js');
 const authRouter = require('./routes/authRouter.js');
-console.log(authRouter)
 
 mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
-// app.use(express.static('lib'));
+app.use(express.static('lib'));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-// app.use('/api', authRouter);
-// app.use('/api', userRouter);
+app.use('/api', userRouter);
+app.use('/api', authRouter);
 
 app.get('/', (req, res) => {
-  return res.sendStatus(200);
+	return res.sendStatus(200);
 });
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () =>{
-  console.log(`listening on ${PORT}`);
+app.listen(PORT, () => {
+	console.log(`listening on ${PORT}`);
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,47 +7,48 @@ const bearerAuth = require('../lib/bearer-auth-middleware.js');
 const userRouter = express.Router();
 
 userRouter.route('/faces')
-  .get(bearerAuth, (req, res) => {
-    console.log('req', req.res);
-    User.find({userId: req.user})
-    .then(users => res.status(200).send(users))
-    .catch(err => res.sendStatus(404).send(err));
-  })
+	.get(bearerAuth, (req, res) => {
+		User.find({
+			_id: req.user._id
+		})
+			.then(users => res.status(200).send(users))
+			.catch(err => res.sendStatus(404).send(err));
+	})
 
-  .post(bearerAuth, (req, res) => {
-    new User(req.body)
-    .save()
-    .then(user => res.status(200).send(user))
-    .catch(err => res.status(500).send(err.message));
-  })
+	.post(bearerAuth, (req, res) => {
+		new User(req.body)
+			.save()
+			.then(user => res.status(200).send(user))
+			.catch(err => res.status(500).send(err.message));
+	});
 
-  userRouter.route('/face/:id')
-    .get(bearerAuth, (req, res) => {
-      if (req.params.id) {
-        return User.findById(req.params.id)
-        .then(user => res.status(200).send(user))
-        .catch(err => res.status(400).send(err.message));
-      };
-    })
+userRouter.route('/face/:id')
+	.get(bearerAuth, (req, res) => {
+		if (req.params.id) {
+			return User.findById(req.params.id)
+				.then(user => res.status(200).send(user))
+				.catch(err => res.status(400).send(err.message));
+		}
+	})
 
-    .put(bearerAuth, (req, res) => {
-      let id = req.params.id;
-      User.findByIdAndUpdate(id, req.body, {
-        new: true
-      })
-      .then(user => res.status(204).send(user))
-      .catch(err => res.status(400).send(err.message));
-    })
+	.put(bearerAuth, (req, res) => {
+		let id = req.params.id;
+		User.findByIdAndUpdate(id, req.body, {
+			new: true
+		})
+			.then(user => res.status(204).send(user))
+			.catch(err => res.status(400).send(err.message));
+	})
 
-    .delete(bearerAuth, (req, res) => {
-      User.findByIdAndRemove(req.params.id)
-      .then(user => {
-        if (website.userId.toString() === req.user.id.toString()) {
-          return user.remove();
-        }
-      })
-      .then(() => res.status(200))
-      .catch(err => res.status(500).send(err.message));
-    });
+	.delete(bearerAuth, (req, res) => {
+		User.findByIdAndRemove(req.params.id)
+			.then(user => {
+				if (website.userId.toString() === req.user.id.toString()) {
+					return user.remove();
+				}
+			})
+			.then(() => res.status(200))
+			.catch(err => res.status(500).send(err.message));
+	});
 
 module.exports = userRouter;
