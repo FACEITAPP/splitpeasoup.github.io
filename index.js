1e081d0d0ce56174389ad578f0ba368830b98978
Caitlin <caitlinsweeneylmt@gmail.com>
2018-04-11 16:30:06 -0700

    Revert "Merge pull request #21 from FACEITAPP/al-day07"
    This reverts commit 1b8966e41dc728a4c79c78a9b9845cf4a565c8b2, reversing
    changes made to ea84c4711b2fdfa8cd8548d45fa84f940fb2c9dd.

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 'use strict';
+require('dotenv').config();
 
-const server = require('./server.js');
-server.start();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/userRouter.js');
+const authRouter = require('./routes/authRouter.js');
+const photoRouter = require('./routes/photoRouter.js');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const app = express();
+
+app.use(express.static('lib'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use('/api', userRouter);
+app.use('/api', authRouter);
+app.use('/api', photoRouter);
+
+// app.get('/', (req, res) => {
+// 	return res.sendStatus(200);
+// });
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log('http://localhost:' + process.env.PORT);
+});
