713a7d71ca6f72cb11da4120954ba934dfa5eedb
scubaguypnw <scubaphoto@gmail.com>
2018-04-11 20:01:17 -0700

    API error handler and API error handler tests both work correctly - all tests pass

--- test/api-error.test.js
+++ test/api-error.test.js
@@ -1,81 +1,59 @@
 const apiError = require('../lib/api-error-handler.js');
 
 
-
 describe('Handle image being too large', () => {
-  it('Should return error: The size of uploaded image does not meet the size requirements.',() => {
-       let result = {
-         error_message : 'IMAGE_FILE_TOO_LARGE'
-       }
-        expect(apiError(result).msg).toBe('The size of uploaded image does not meet the size requirements.');
-        });
+  it('Should return error: The size of uploaded image does not meet the size requirements.', () => {
+    let result = {
+      error_message: 'IMAGE_FILE_TOO_LARGE'
+    }
+    expect(apiError(result).msg).toBe('The image file is too large. This API requires image file size to be no larger than 2 MB.');
   });
+});
 
-  describe('Handle image unsuported format', () => {
-    it('Should return error: IMAGE_FILE_TOO_LARGE',() => {
-         let result = {
-           error_message : 'IMAGE_ERROR_UNSUPPORTED_FORMAT'
-         }
-          expect(apiError(result).msg).toBe('The size of uploaded image does not meet the size requirements.');
-          });
-    });
+describe('Handle image unsuported format', () => {
+  it('Should return error: IMAGE_ERROR_UNSUPPORTED_FORMAT', () => {
+    let result = {
+      error_message: 'IMAGE_ERROR_UNSUPPORTED_FORMAT'
+    }
+    expect(apiError(result).msg).toBe('The uploaded image can not be resolved. The file format may not be supported or the file is damaged.');
+  });
+});
+
+describe('Handle image size does not meet the requirement', () => {
+  it('Should return error: INVALID_IMAGE_SIZE', () => {
+    let result = {
+      error_message: 'INVALID_IMAGE_SIZE'
+    }
+    expect(apiError(result).msg).toBe('The size of uploaded image does not meet the size requirements.');
+  });
+});
+
+describe('Handle invalid image URL', () => {
+  it('Should return error: INVALID_IMAGE_URL', () => {
+    let result = {
+      error_message: 'INVALID_IMAGE_URL'
+    }
+    expect(apiError(result).msg).toBe('Failed downloading image from URL. The image URL is wrong or invalid.');
+  });
+});
+
+describe('Handle image download timing out', () => {
+  it('Should return error:IMAGE_DOWNLOAD_TIMEOUT', () => {
+    let result = {
+      error_message: 'IMAGE_DOWNLOAD_TIMEOUT'
+    }
+    expect(apiError(result).msg).toBe('IMAGE_DOWNLOAD_TIMEOUT');
+  });
+});
+
+describe('Handle insufficient permission to use API', () => {
+  it('Should return error: INSUFFICIENT_PERMISSION', () => {
+    let result = {
+      error_message: 'INSUFFICIENT_PERMISSION'
+    }
+    expect(apiError(result).msg).toBe('With a Free API Key, you cannot use this. Please do not use this parameter, or use with a Standard API Key.');
+  });
+});
 
 
-  // if (result.error_message.includes('IMAGE_ERROR_UNSUPPORTED_FORMAT')) {
-	// 	console.log("400 - IMAGE_ERROR_UNSUPPORTED_FORMAT");
-	// 	return {
-	// 		msg: 'The uploaded image can not be resolved. The file format may not be supported or the file is damaged.',
-	// 		status: 400
-	// 	};
-	// }
 
-
-  
-
-
-
-// let apiError = function (result, res) {
-// 	if (result.error_message.includes('IMAGE_ERROR_UNSUPPORTED_FORMAT')) {
-// 		console.log("400 - IMAGE_ERROR_UNSUPPORTED_FORMAT");
-// 		return {
-// 			msg: 'The uploaded image can not be resolved. The file format may not be supported or the file is damaged.',
-// 			status: 400
-// 		};
-// 	}
-
-// 	if (result.error_message.includes('INVALID_IMAGE_SIZE')) {
-// 		console.log("400 - INVALID_IMAGE_SIZE");
-// 		return {
-// 			msg: 'The size of uploaded image does not meet the size requirements.',
-// 			status: 400
-// 		};
-// 	}
-
-// 	if (result.error_message.includes('INVALID_IMAGE_URL')) {
-// 		console.log('400 - INVALID_IMAGE_URL');
-// 		return { 
-//       msg : 'Failed downloading image from URL. The image URL is wrong or invalid.',
-//       status: 400
-//     }
-//   }
-
-// 	if (result.error_message.includes('IMAGE_FILE_TOO_LARGE')) {
-// 		console.log('400 - IMAGE_FILE_TOO_LARGE');
-// 		return {
-// 			msg: 'The image file is too large. This API requires image file size to be no larger than 2 MB.',
-// 			status: 400
-// 		};
-// 	}
-
-// 	if (result.error_message.includes('INSUFFICIENT_PERMISSION')) {
-// 		res.status(403).send('With a Free API Key, you cannot use this. Please do not use this parameter, or use with a Standard API Key.')
-// 		console.log('403 - 	INSUFFICIENT_PERMISSION');
-// 	}
-
-// 	if (result.error_message.includes('IMAGE_DOWNLOAD_TIMEOUT')) {
-// 		res.status(412).send('With a Free API Key, you cannot use this. Please do not use this parameter, or use with a Standard API Key.')
-// 		console.log('412 -IMAGE_DOWNLOAD_TIMEOUT');
-// 	}
-// };
-
-// module.exports = apiError;
