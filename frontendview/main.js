72f8020b1532124a4627f05d214668c161ef6452
alicialycan <alicialycan@gmail.com>
2018-04-11 19:39:42 -0700

    stopping FORM and using AJAX to send request with headers

--- frontendview/main.js
+++ frontendview/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 console.log('hello');
+
+const signInForm = document.getElementById("signinForm");
+signInForm.addEventListener("submit", (ev) => {
+  console.log("submitting sign in");
+  ev.preventDefault();
+
+  const url = signInForm.getAttribute('action');
+  const method = signInForm.getAttribute('method');
+
+  let username = 'bob';    // TODO: grab username/pass from form
+  let password = 'marley';
+  let payload = btoa(username + ':' + password);
+
+  console.log("submitting sign in to", method, url);
+  fetch(url, {
+    method: method,
+    headers: {
+      'Authorization': 'Basic ' + payload
+    }
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(json => {
+    console.log('json:', json);
+    localStorage.setItem('token', json.token);
+  })
+  .catch(err => {
+    console.log("err:", err);
+  })
+});
+
